00e8b9ada8583c6ad5dd97ca73f3c9b4bee8000e
hiralshah7 <90278193+hiralshah7@users.noreply.github.com>
2022-11-01 11:42:21 -0400

    adding_fetch_call
    adding fetch call

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
     // create vue instance here
     const { createApp } = Vue
 
+    // this will be a fetch call which will always be same 
+    // if this is call from index.php then it will be ./includes/index.php
+
     createApp({
         created() {
-            
+            // fetch calls always go here : bringing any remote data you need+
+            fetch('./data.json') // will go and will get the remote data
+                .then(res => res.json()) // convert the json object to plane js object
+                .then(data => this.heroData = data) // store the data in the vue instance or the console instance
+                .catch(error => console.error(error)); // report any error that might occur
+
         },
 
         data() {
