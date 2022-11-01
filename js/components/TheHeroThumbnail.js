e2916acbc40f7cad061893b28c8f71e00233dd08
hiralshah7 <90278193+hiralshah7@users.noreply.github.com>
2022-11-01 11:57:50 -0400

    adding_component
    adding component in index.htm and then pasting it in the component for vue js

---
+++ js/components/TheHeroThumbnail.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'TheHeroThumb',
+
+    props: {
+        hero: Object
+    },
+    template:
+
+        `
+    <li>
+            <!-- <img class="sprite" src="" alt="hero image"> -->
+            <div class="sprite"></div>
+            <div class="red-bumper"></div>
+            <h5>name</h5>
+    </li>
+`
+}
