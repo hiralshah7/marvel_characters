1003979be874b9587abad705b706967e03641bfc
hiralshah7 <90278193+hiralshah7@users.noreply.github.com>
2022-11-08 13:42:17 -0500

    adding_text

--- js/components/ThelightboxComponent.js
+++ js/components/ThelightboxComponent.js
@@ -10,9 +10,9 @@ export default {
     <!-- hero image on the left, text on the right -->
     <!-- her image is a background image -->
     <article>
-        <h3 class="lb_heading">Captain America - the First Avenger</h3>
+        <h3 class="lb_heading">{{ hero.name }}</h3>
         <p class="lb_text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum autem pariatur dicta eum dolorum nulla neque laborum praesentium quo possimus, mollitia, dolor omnis rerum dolorem ratione in natus? Nihil, mollitia!
+          {{ hero.id }}
         </p>
         <p class="lb_text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quaerat totam, unde quam aspernatur quasi delectus sed earum nobis voluptate voluptas suscipit repudiandae explicabo placeat numquam error. Praesentium, quisquam illum.
